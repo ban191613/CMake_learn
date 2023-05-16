a88b72cafb12bd7f90ff74b14789e3ddc4b6ee53
wjf <ban191613@163.com>
2023-05-16 22:02:26 +0800

    cmak_value

---
+++ build/src/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/git_Warehouse/CMake_learn/src/main.cpp" "src/CMakeFiles/test.dir/main.cpp.obj" "gcc" "src/CMakeFiles/test.dir/main.cpp.obj.d"
+  "D:/git_Warehouse/CMake_learn/src/maincopy.cpp" "src/CMakeFiles/test.dir/maincopy.cpp.obj" "gcc" "src/CMakeFiles/test.dir/maincopy.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
